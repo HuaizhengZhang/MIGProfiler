05e17e7ca84e8af1e575866bb5ab5f51f4dc0801
YuanmingLeee <YuanmingLeee@gmail.com>
2022-12-21 18:17:03 +0800

    :books: [exp] Re-org and add doc for profiling MIG with different seq length

--- exp/gpu_perf/seq_length/mig_diff_gi_seq_len_async_nlp.sh
+++ exp/gpu_perf/seq_length/mig_diff_gi_seq_len_async_nlp.sh
@@ -2,15 +2,17 @@
 GPU_ID=0
 MODEL_NAME='bert-base-cased'
 NUM_TEST_BATCHES=1000
-# MIG_PROFILES=('1g.10gb' '2g.20gb' '3g.40gb' '4g.40gb' '7g.80gb')
-MIG_PROFILES=('3g.40gb' '4g.40gb' '7g.80gb')
+MIG_PROFILES=('1g.10gb' '2g.20gb' '3g.40gb' '4g.40gb' '7g.80gb')
 SEQ_LENS=(32 64 128 256)
 NUM_THREADS=4
 BATCH_SIZE=32
 
-EXP_SAVE_DIR="${PWD}"
-cd ../../mig_perf/inference
-export PYTHONPATH="${PWD}"
+BASE_DIR=$(realpath $0 | xargs dirname)
+EXP_SAVE_DIR="${BASE_DIR}/seq_length/async_request"
+PYTHON_EXECUTION_ROOT="${BASE_DIR}/../../../mig_perf/inference"
+DCGM_EXPORTER_METRICS_PATH="${PYTHON_EXECUTION_ROOT}/client/dcp-metrics-included.csv:/etc/dcgm-exporter/customized.csv"
+cd "${PYTHON_EXECUTION_ROOT}"
+export PYTHONPATH="${PYTHON_EXECUTION_ROOT}"
 
 echo 'Enable MIG'
 sudo nvidia-smi -i "${GPU_ID}" -mig 1
@@ -19,12 +21,13 @@ for MIG_PROFILE in "${MIG_PROFILES[@]}"; do
   echo '=========================================================='
   echo " * MIG PROFILE = ${MIG_PROFILE}"
   echo '=========================================================='
+  echo 'Create MIG instances'
   sudo nvidia-smi mig -i 0 -cgi "${MIG_PROFILE}" -C
   sleep 5
 
   echo 'Start DCGM'
   docker run -d --rm --gpus all --net mig_perf -p 9400:9400  \
-    -v "${EXP_SAVE_DIR}/../../mig_perf/inference/client/dcp-metrics-included.csv:/etc/dcgm-exporter/customized.csv" \
+    -v "${DCGM_EXPORTER_METRICS_PATH}" \
     --name dcgm_exporter --cap-add SYS_ADMIN   nvcr.io/nvidia/k8s/dcgm-exporter:2.4.7-2.6.11-ubuntu20.04 \
     -c 500 -f /etc/dcgm-exporter/customized.csv -d f
   sleep 3
@@ -36,7 +39,7 @@ for MIG_PROFILE in "${MIG_PROFILES[@]}"; do
     echo 'Start profiling client 0'
     python client/block_inference_nlp.py -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n "${NUM_TEST_BATCHES}" \
       -t "${NUM_THREADS}" --seq_len "${SEQ_LEN}" \
-      -i "${GPU_ID}" -mi 0 -dbn "${EXP_SAVE_DIR}/seq_length/async_block_request/${MIG_PROFILE}" --report-suffix "seq${SEQ_LEN}"
+      -i "${GPU_ID}" -mi 0 -dbn "${EXP_SAVE_DIR}/${MIG_PROFILE}" --report-suffix "seq${SEQ_LEN}"
 
     echo 'Finish!'
     sleep 10
@@ -44,7 +47,7 @@ for MIG_PROFILE in "${MIG_PROFILES[@]}"; do
 
   echo 'Stop DCGM'
   docker stop dcgm_exporter
-
+  echo 'Destroy MIG instances'
   sudo nvidia-smi mig -i "${GPU_ID}" -dci
   sudo nvidia-smi mig -i "${GPU_ID}" -dgi
 
@@ -52,3 +55,5 @@ for MIG_PROFILE in "${MIG_PROFILES[@]}"; do
 done
 echo 'Disable MIG'
 sudo nvidia-smi -i "${GPU_ID}" -mig 0
+echo 'Reset GPU'
+sudo nvidia-smi -i "${GPU_ID}" -r
--- exp/gpu_perf/seq_length/no_mig_seq_len_async_nlp.sh
+++ exp/gpu_perf/seq_length/no_mig_seq_len_async_nlp.sh
@@ -5,14 +5,21 @@ NUM_TEST_BATCHES=1000
 BATCH_SIZE=32
 SEQ_LENS=(32 64 128 256)
 NUM_THREADS=4
+MIG_PROFILE='no_mig'
 
-EXP_SAVE_DIR="${PWD}"
-cd ../../mig_perf/inference
-export PYTHONPATH="${PWD}"
+BASE_DIR=$(realpath $0 | xargs dirname)
+EXP_SAVE_DIR="${BASE_DIR}/seq_length/async_request"
+PYTHON_EXECUTION_ROOT="${BASE_DIR}/../../../mig_perf/inference"
+DCGM_EXPORTER_METRICS_PATH="${PYTHON_EXECUTION_ROOT}/client/dcp-metrics-included.csv:/etc/dcgm-exporter/customized.csv"
+cd "${PYTHON_EXECUTION_ROOT}"
+export PYTHONPATH="${PYTHON_EXECUTION_ROOT}"
 
+echo '=========================================================='
+echo " * MIG PROFILE = ${MIG_PROFILE}"
+echo '=========================================================='
 echo 'Start DCGM'
 docker run -d --rm --gpus all --net mig_perf -p 9400:9400  \
-  -v "${EXP_SAVE_DIR}/../../mig_perf/inference/client/dcp-metrics-included.csv:/etc/dcgm-exporter/customized.csv" \
+  -v "${DCGM_EXPORTER_METRICS_PATH}" \
   --name dcgm_exporter --cap-add SYS_ADMIN   nvcr.io/nvidia/k8s/dcgm-exporter:2.4.7-2.6.11-ubuntu20.04 \
   -c 500 -f /etc/dcgm-exporter/customized.csv -d f
 sleep 3
@@ -23,7 +30,7 @@ for SEQ_LEN in "${SEQ_LENS[@]}"; do
   echo "Batch size ${SEQ_LEN}"
   echo 'Start profiling client 0'
   python client/block_inference_nlp.py -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n "${NUM_TEST_BATCHES}" -t "${NUM_THREADS}" \
-    -i "${GPU_ID}" --seq_len "${SEQ_LEN}" -dbn "${EXP_SAVE_DIR}/seq_length/async_block_request/no_mig"
+    -i "${GPU_ID}" --seq_len "${SEQ_LEN}" -dbn "${EXP_SAVE_DIR}/${MIG_PROFILE}"
 
   echo 'Finish!'
   sleep 10
