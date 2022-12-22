2944494dd246af281758cb61a813ac2f7bfc0510
yuanmingleee <yli056@e.ntu.edu.sg>
2022-12-22 23:05:19 +0800

    :recycle: [exp] Update multi MIG instance exp scripts

--- exp/mig/mig_1x4_model_name_cv.sh
+++ exp/mig/mig_1x4_model_name_cv.sh
@@ -1,25 +1,25 @@
 #! /usr/bin/env bash
-### Disable MPS, enable MIG
-# echo quit | nvidia-cuda-mps-control
-# sudo nvidia-smi -i 0 -mig 1
-# sudo nvidia-smi mig -i 0 -cgi 1g.6gb,1g.6gb,1g.6gb,1g.6gb -C
 GPU_ID=0
 MODEL_NAMES=('resnet18' 'resnet34' 'resnet50' 'resnet101')
 NUM_TEST_BATCHES=1000
 BATCH_SIZE=32
 
-EXP_SAVE_DIR="${PWD}"
-cd ../../mig_perf/inference
-export PYTHONPATH="${PWD}"
+BASE_DIR=$(realpath $0 | xargs dirname)
+EXP_SAVE_DIR="${BASE_DIR}/model_name1x4"
+PYTHON_EXECUTION_ROOT="${BASE_DIR}/../../mig_perf/inference"
+DCGM_EXPORTER_METRICS_PATH="${PYTHON_EXECUTION_ROOT}/client/dcp-metrics-included.csv:/etc/dcgm-exporter/customized.csv"
+cd "${PYTHON_EXECUTION_ROOT}"
+export PYTHONPATH="${PYTHON_EXECUTION_ROOT}"
 
 echo 'Enable MIG'
 sudo nvidia-smi -i "${GPU_ID}" -mig 1
+echo 'Create MIG instances'
 sudo nvidia-smi mig -cgi 1g.6gb,1g.6gb,1g.6gb,1g.6gb -C
 
 echo 'Start DCGM'
 docker run -d --rm --gpus all --net mig_perf -p 9400:9400  \
-  -v "${EXP_SAVE_DIR}/../../mig_perf/inference/client/dcp-metrics-included.csv:/etc/dcgm-exporter/customized.csv" \
-  --name dcgm_exporter --cap-add SYS_ADMIN   nvcr.io/nvidia/k8s/dcgm-exporter:2.4.7-2.6.11-ubuntu20.04 \
+  -v "${DCGM_EXPORTER_METRICS_PATH}:/etc/dcgm-exporter/customized.csv" \
+  --name dcgm_exporter --cap-add SYS_ADMIN nvcr.io/nvidia/k8s/dcgm-exporter:2.4.7-2.6.11-ubuntu20.04 \
   -c 500 -f /etc/dcgm-exporter/customized.csv -d f
 sleep 3
 docker ps
@@ -27,29 +27,31 @@ docker ps
 # iterate through batch size list
 for MODEL_NAME in "${MODEL_NAMES[@]}"; do
     echo "Batch size ${BATCH_SIZE}"
-    echo 'Start dummy client 1'
-    python client/block_inference_cv.py --dry-run  -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n 1048575  \
-      -i "${GPU_ID}" -mi 1  > /dev/null 2>&1 &
+    echo 'Start client 1'
+    python client/block_inference_cv.py  -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n 1048575  \
+      -i "${GPU_ID}" -mi 1 -dbn "${EXP_SAVE_DIR}" --report-suffix client0  > /dev/null 2>&1 &
     CLIENT1_PID=$!
 
-    echo 'Start dummy client 2'
-    python client/block_inference_cv.py --dry-run  -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n 1048575  \
-      -i "${GPU_ID}" -mi 2  > /dev/null 2>&1 &
+    echo 'Start client 2'
+    python client/block_inference_cv.py  -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n 1048575  \
+      -i "${GPU_ID}" -mi 2 -dbn "${EXP_SAVE_DIR}" --report-suffix client0  > /dev/null 2>&1 &
     CLIENT2_PID=$!
 
-    echo 'Start dummy client 3'
-    python client/block_inference_cv.py --dry-run  -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n 1048575  \
-      -i "${GPU_ID}" -mi 3  > /dev/null 2>&1 &
+    echo 'Start client 3'
+    python client/block_inference_cv.py  -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n 1048575  \
+      -i "${GPU_ID}" -mi 3 -dbn "${EXP_SAVE_DIR}" --report-suffix client0  > /dev/null 2>&1 &
     CLIENT3_PID=$!
 
     sleep 5
 
     echo 'Start profiling client 0'
     python client/block_inference_cv.py -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n "${NUM_TEST_BATCHES}" \
-      -i "${GPU_ID}" -mi 0  -dbn "${EXP_SAVE_DIR}/model_name1x4"
+      -i "${GPU_ID}" -mi 0 -dbn "${EXP_SAVE_DIR}" --report-suffix client0
 
-    echo 'Cleaning up...'
-    kill -9 $CLIENT1_PID $CLIENT2_PID $CLIENT3_PID
+    echo 'Wait client to finish...'
+    wait $CLIENT1_PID
+    wait $CLIENT2_PID
+    wait $CLIENT3_PID
 
     echo 'Finish!'
 
@@ -59,7 +61,10 @@ done
 echo 'Stop DCGM'
 docker stop dcgm_exporter
 
-echo 'Disable MIG'
+echo 'Destroy MIG instances'
 sudo nvidia-smi mig -i "${GPU_ID}" -dci
 sudo nvidia-smi mig -i "${GPU_ID}" -dgi
+echo 'Disable MIG'
 sudo nvidia-smi -i "${GPU_ID}" -mig 0
+echo 'Reset GPU'
+sudo nvidia-smi -i "${GPU_ID}" -r
--- exp/mig/mig_4x1_batch_size_cv.sh
+++ exp/mig/mig_4x1_batch_size_cv.sh
@@ -4,18 +4,22 @@ MODEL_NAME='resnet18'
 NUM_TEST_BATCHES=1000
 BATCH_SIZES=(1 2 4 8 16 32 64)
 
-EXP_SAVE_DIR="${PWD}"
-cd ../../mig_perf/inference
-export PYTHONPATH="${PWD}"
+BASE_DIR=$(realpath $0 | xargs dirname)
+EXP_SAVE_DIR="${BASE_DIR}/batch_size4x1"
+PYTHON_EXECUTION_ROOT="${BASE_DIR}/../../mig_perf/inference"
+DCGM_EXPORTER_METRICS_PATH="${PYTHON_EXECUTION_ROOT}/client/dcp-metrics-included.csv:/etc/dcgm-exporter/customized.csv"
+cd "${PYTHON_EXECUTION_ROOT}"
+export PYTHONPATH="${PYTHON_EXECUTION_ROOT}"
 
 echo 'Enable MIG'
 sudo nvidia-smi -i "${GPU_ID}" -mig 1
+echo 'Create MIG instances'
 sudo nvidia-smi mig -cgi 4g.24gb -C
 
 echo 'Start DCGM'
 docker run -d --rm --gpus all --net mig_perf -p 9400:9400  \
-  -v "${EXP_SAVE_DIR}/../../mig_perf/inference/client/dcp-metrics-included.csv:/etc/dcgm-exporter/customized.csv" \
-  --name dcgm_exporter --cap-add SYS_ADMIN   nvcr.io/nvidia/k8s/dcgm-exporter:2.4.7-2.6.11-ubuntu20.04 \
+  -v "${DCGM_EXPORTER_METRICS_PATH}:/etc/dcgm-exporter/customized.csv" \
+  --name dcgm_exporter --cap-add SYS_ADMIN nvcr.io/nvidia/k8s/dcgm-exporter:2.4.7-2.6.11-ubuntu20.04 \
   -c 500 -f /etc/dcgm-exporter/customized.csv -d f
 sleep 3
 docker ps
@@ -25,7 +29,7 @@ for BATCH_SIZE in "${BATCH_SIZES[@]}"; do
     echo "Batch size ${BATCH_SIZE}"
     echo 'Start profiling client 0'
     python client/block_inference_cv.py -b "${BATCH_SIZE}" -m "${MODEL_NAME}" -n "${NUM_TEST_BATCHES}" \
-      -i "${GPU_ID}" -mi 0  -dbn "${EXP_SAVE_DIR}/batch_size4x14" --report-suffix client0
+      -i "${GPU_ID}" -mi 0  -dbn "${EXP_SAVE_DIR}" --report-suffix client0
 
     echo 'Finish!'
 
@@ -35,7 +39,10 @@ done
 echo 'Stop DCGM'
 docker stop dcgm_exporter
 
-echo 'Disable MIG'
+echo 'Destroy MIG instances'
 sudo nvidia-smi mig -i "${GPU_ID}" -dci
 sudo nvidia-smi mig -i "${GPU_ID}" -dgi
+echo 'Disable MIG'
 sudo nvidia-smi -i "${GPU_ID}" -mig 0
+echo 'Reset GPU'
+sudo nvidia-smi -i "${GPU_ID}" -r
