9505ab97734f78d7e897f083d43d89543297d123
yuanmingleee <yli056@e.ntu.edu.sg>
2022-12-31 23:19:54 +0800

    :fire: Remove unused files

--- mig_perf/profiler/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-WORK_SPACE = Path("/workspace/data/")
-PLACES365_DATA_DIR = WORK_SPACE / "places365_standard"
-RESULT_DATA_DIR = WORK_SPACE / "results"
-TORCH_HOME = WORK_SPACE / "torch_models"
-HF_HOME = WORK_SPACE / "huggingface"
