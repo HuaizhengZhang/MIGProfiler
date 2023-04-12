db6bb10603f2bbf63ca845b195a379e32dcf549a
yuanmingleee <yli056@e.ntu.edu.sg>
2023-04-12 23:47:32 +0800

    :recycle: [refactor] Re-org profiler code structure
    profiler client, server -> profiler/inference
    profiler monitor related -> ../dcgm_exporter

---
+++ migperf/dcgm_exporter/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Li Yuanming
+Email: yuanmingleee@gmail.com
+Date: Apr 12, 2023
+"""
+from metric_collector import DCGMMetricCollector
+
+
+__all__ = ['DCGMMetricCollector']
---
+++ migperf/profiler/inference/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Author: Li Yuanming
+Email: yuanmingleee@gmail.com
+Date: Apr 12, 2023
+"""
